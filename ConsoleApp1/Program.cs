77dac4ac53e1cf0ccd084d51cbb305584d4cf546
Abdullah Gazi Tapan <gazitapan@outlook.com>
2022-06-07 13:03:29 +0300

    C# ile buyukten kucuge siralama

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace buyuktenKucuge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                int sayi = 0;
+                Console.WriteLine($"{i + 1}. Sayıyı Giriniz:");
+                if (!int.TryParse(Console.ReadLine(), out sayi))
+                    throw new Exception("Lütfen Sayı Giriniz");
+                arr.Add(sayi);
+            }
+
+            var desc = arr.OrderByDescending(f => f).ToList();
+            foreach (var item in desc)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
